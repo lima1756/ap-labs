4d2f4846f97180663b1d42c487c3da24ffbd3283
Ivan Morett <luisivanmorett@gmail.com>
2020-02-15 17:10:05 -0600

    logger

---
+++ labs/logger/logger.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+#define INFO_COLOR 37
+#define WARNING_COLOR 33
+#define ERROR_COLOR 31
+#define PANIC_FONT_COLOR 30
+#define PANIC_BACK_COLOR 41
+#define DEFAULT_BACK_COLOR 48
+#define DEFAULT_FONT_COLOR 37
+
+void setColor(int font, int back) {
+	printf("%c[0;%d;%dm",0x1B, font, back);
+}
+
+void resetColor() {
+    setColor(DEFAULT_FONT_COLOR, DEFAULT_BACK_COLOR);
+}
+
+void print(const char *format, va_list args) {
+    vprintf(format, args);
+}
+
+int infof(const char *format, ...){
+    setColor(INFO_COLOR, DEFAULT_BACK_COLOR);
+    va_list args;
+    va_start(args, format);
+    print(format, args);
+    va_end(args);
+    resetColor();
+}
+
+int warnf(const char *format, ...) {
+    setColor(WARNING_COLOR, DEFAULT_BACK_COLOR);
+    va_list args;
+    va_start(args, format);
+    print(format, args);
+    va_end(args);
+    resetColor();
+}
+
+int errorf(const char *format, ...) {
+    setColor(ERROR_COLOR, DEFAULT_BACK_COLOR);
+    va_list args;
+    va_start(args, format);
+    print(format, args);
+    va_end(args);
+    resetColor();
+}
+
+int panicf(const char *format, ...) {
+    setColor(PANIC_FONT_COLOR, PANIC_BACK_COLOR);
+    va_list args;
+    va_start(args, format);
+    print(format, args);
+    va_end(args);
+    resetColor();
+}
+
