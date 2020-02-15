c03503d58a2616b9c45242923a9e6da6dd3c4dc5
Ivan Morett <luisivanmorett@gmail.com>
2020-02-15 17:10:05 -0600

    logger

--- labs/logger/testLogger.c
+++ labs/logger/testLogger.c
@@ -1,5 +1,9 @@
-
+#include "logger.h"
 
 int main() {
+    infof("This is info: %d\n", 5);
+    warnf("This is warning: %c\n", 'c');
+    errorf("This is error: %d\n", 3.14);
+    panicf("This is panic: %s\n", "AAAAAAAA");
     return 0;
 }
