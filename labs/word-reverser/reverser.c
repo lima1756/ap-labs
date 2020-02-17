68413631e47e44823cd88934b49bd4bc0abd5944
Ivan Morett <luisivanmorett@gmail.com>
2020-02-17 17:56:52 -0600

    word-reverser

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 1000
 
 int main(){
-    // Place your magic here
+    char c;
+    char word[1000];
+    int count = 0;
+    while((c = getchar()) != EOF){
+        if(c=='\n') {
+            count--;
+            while(count>=0) {
+                printf("%c", word[count--]);
+            }
+            count = 0;
+            printf("\n");
+        }
+        else {
+            word[count++]=c;
+        }
+    }    
+    
     return 0;
 }
