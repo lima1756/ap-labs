acdce0586852b840debc43a09a3b1cd9f10ea843
Ivan Morett <luisivanmorett@gmail.com>
2020-02-17 16:31:11 -0600

    cat2

--- labs/mycat/cat2.c
+++ labs/mycat/cat2.c
@@ -1,37 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include <fcntl.h>  
+#include <string.h>
+
+#define BUFFER 255
+
+size_t read (int fd, void* buf, size_t cnt);  
+
 
 /* filecopy:  copy file ifp to file ofp */
-void filecopy(FILE *ifp, FILE *ofp)
+void filecopy(int ifp, int ofp)
 {
-    int c;
-
-    while ((c = getc(ifp)) != EOF)
-        putc(c, ofp);
-
+    char characters[BUFFER];
+    int count;
+    while ((count = read(ifp, characters, BUFFER)) > 0){
+        if(write(ofp, characters, count) != count){
+            write(2, "Error while writing\n", 21);
+        }
+       
+    }
 }
 
 /* cat:  concatenate files, version 2 */
 int main(int argc, char *argv[])
 {
-    FILE *fp;
-    void filecopy(FILE *, FILE *);
+    int fp;
     char *prog = argv[0];   /* program name for errors */
 
     if (argc == 1)  /* no args; copy standard input */
-        filecopy(stdin, stdout);
+        filecopy(0, 1);
     else
-        while (--argc > 0)
-            if ((fp = fopen(*++argv, "r")) == NULL) {
-                fprintf(stderr, "%s: can′t open %s\n",
-			prog, *argv);
+        while (--argc > 0) {
+            if ((fp = open(*++argv, O_RDONLY)) == -1) {
+                write(2, prog, strlen(prog));
+                write(2, ": can′t open ", 13);
+                write(2, *argv, strlen(*argv));
+                write(2, "\n", 1);
                 return 1;
             } else {
-                filecopy(fp, stdout);
-                fclose(fp);
+                filecopy(fp, 1);
+                close(fp);
             }
+        }
 
     if (ferror(stdout)) {
-        fprintf(stderr, "%s: error writing stdout\n", prog);
+        write(2, prog, strlen(prog));
+        write(2, ": error writing stdout\n", 24);
         return 2;
     }
 
