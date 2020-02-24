76c080c0cb57ebeeb697a50fbe5e9f0ceb1f6138
Ivan Morett <luisivanmorett@gmail.com>
2020-02-23 20:38:12 -0600

    exercises

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"strings"
+
+	"golang.org/x/tour/wc"
+)
+
+func WordCount(s string) map[string]int {
+	data := make(map[string]int)
+	for _, val := range strings.Fields(s) {
+		data[val]++
+	}
+	return data
+}
+
 func main() {
+	wc.Test(WordCount)
 }
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,4 +1,20 @@
 package main
 
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	data := make([][]uint8, dy)
+	for i := 0; i < dy; i++ {
+		data[i] = make([]uint8, dx)
+	}
+	for y := 0; y < dy; y++ {
+		for x := 0; x < dx; x++ {
+			data[y][x] = uint8(((x + y) / 2) % 255)
+		}
+	}
+	return data
+}
+
 func main() {
+	pic.Show(Pic)
 }
