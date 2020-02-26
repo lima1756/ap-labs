8605fa4b8381c5718b33dd5eb111d5aa26347510
Ivan Morett <luisivanmorett@gmail.com>
2020-02-26 14:53:57 -0600

    hello world?

--- labs/openmp/hello_par.c
+++ labs/openmp/hello_par.c
@@ -1,4 +1,11 @@
+#include <stdio.h>
+#include <omp.h>
+#include "logger.h"
 
 int main() {
+    #pragma omp parallel
+    {
+        infof("Hello from process: %d\n", omp_get_thread_num());
+    }
     return 0;
 }
